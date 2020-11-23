04b3bd8bfee5355e7aedbd71d4be48f7c7428fa9
ankita <ankimitr@cisco.com>
2020-11-23 17:37:10 +0530

    id in string form

--- src/main/java/com/practice/mongo/TaskControlller.java
+++ src/main/java/com/practice/mongo/TaskControlller.java
@@ -21,8 +21,11 @@ import javax.ws.rs.core.Response;
 import org.bson.Document;
 import org.bson.types.ObjectId;
 
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
 
@@ -41,26 +44,25 @@ public class TaskControlller {
     @Path("/tasks")
     @Produces({MediaType.APPLICATION_JSON})
     public Response getPosts() {
-    	List<Task> result = collection.find().into(new ArrayList<Task>());
-        return Response.status(200).entity(result).build();
+        return Response.status(200).entity(getTaskList()).build();
     }
     
     @POST
     @Path("/tasks/")
+    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Response createTask(Task _task) {
     	 collection.insertOne(new Task( _task.getTitle()));
-	   	 List<Task> result = collection.find().into(new ArrayList<Task>());
-	     return Response.status(200).entity(result).build();
+    	 return Response.status(200).entity(getTaskList()).build();
     }
     
     @PUT
     @Path("/tasks/{id}")
+    @Consumes({MediaType.APPLICATION_JSON})
     @Produces({MediaType.APPLICATION_JSON})
     public Response updateTask(@PathParam("id") String id, Task _task) {
 	   	 collection.updateOne(Filters.eq("_id", new ObjectId(id)), set("title",  _task.getTitle()));
-	   	 List<Task> result = collection.find().into(new ArrayList<Task>());
-	     return Response.status(200).entity(result).build();
+	   	 return Response.status(200).entity(getTaskList()).build();
     }
     
     @DELETE
@@ -68,7 +70,24 @@ public class TaskControlller {
     @Produces({MediaType.APPLICATION_JSON})
     public Response deleteTask(@PathParam("id") String id) {
     	 collection.deleteOne(Filters.eq("_id", new ObjectId(id)));
-    	 List<Task> result = collection.find().into(new ArrayList<Task>());
-        return Response.status(200).entity(result).build();
+    	 return Response.status(200).entity(getTaskList()).build();
     }
+    
+    public List getTaskList() {
+    	FindIterable<Task> _tasks = collection.find();
+    	MongoCursor<Task> cursor = _tasks.iterator();
+    	
+        List result = new ArrayList();
+        
+        while(cursor.hasNext()) {
+       	 BasicDBObject dbObject = new BasicDBObject();
+       	 Task item = cursor.next();
+       	 
+       	 dbObject.append("id", item.getId().toString());
+       	 dbObject.append("title", item.getTitle());
+       	 
+       	 result.add(dbObject);
+        }
+		return result;
+    } 
 }
